160a1ae42408eb34448d8380f7e35e9e8397e9d0
Regorlas <roger.random@outlook.com>
2023-03-22 03:19:30 +0000

    Serve index.html with serve-static

---
+++ magnetometer/tools/serve.js
@@ -0,0 +1,13 @@
+// Example from https://www.npmjs.com/package/serve-static
+
+const finalhandler = require('finalhandler');
+const http = require('http');
+const serveStatic = require('serve-static');
+
+const serve = serveStatic('./src/', {index: ['index.html']});
+
+const server = http.createServer(function onRequest (req, res) {
+  serve(req, res, finalhandler(req, res))
+});
+
+server.listen(8765);
