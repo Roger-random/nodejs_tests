0367d1fd2a14ba83b79bfc0434cc466169ac7017
Regorlas <roger.random@outlook.com>
2023-03-22 16:43:50 -0700

    onerror/onreading/start!

--- magnetometer/src/script.js
+++ magnetometer/src/script.js
@@ -1,13 +1,40 @@
 "use strict";
-let textout;
+let statusText;
 let mag;
+function magnetometerReading() {
+    if (statusText) {
+        statusText.textContent = `Magnetometer Reading Callback: X=${mag.x} Y=${mag.y} Z=${mag.z} @ time=${mag.timestamp}`;
+    }
+}
+function magnetometerError(e) {
+    if (statusText) {
+        statusText.textContent = `Magnetometer Error Callback: ${e.error.message}`;
+    }
+}
 function contentLoaded() {
-    console.log("Content loaded!");
-    textout = document.getElementById('textout');
-    if (null !== textout) {
-        textout.textContent = `typeof(Gyroscope) is ${typeof (Gyroscope)} and typeof(Magnetometer) is ${typeof (Magnetometer)}`;
-        mag = new Magnetometer();
-        textout.textContent = `Magnetometer activated=${mag.activated} and hasReading=${mag.hasReading}`;
+    statusText = document.getElementById('statusText');
+    if (statusText) {
+        if ('undefined' !== typeof (Magnetometer)) {
+            try {
+                mag = new Magnetometer({ frequency: 10 });
+                statusText.textContent = `Magnetometer Created`;
+                mag.onerror = magnetometerError;
+                mag.onreading = magnetometerReading;
+                mag.start();
+                statusText.textContent = `Magnetometer Started`;
+            }
+            catch (e) {
+                if (e && typeof (e) === 'object' && 'message' in e) {
+                    statusText.textContent = `Magnetometer Exception: ${e.message}`;
+                }
+                else {
+                    statusText.textContent = 'Magnetometer Exception raised with no diagnostic message';
+                }
+            }
+        }
+        else {
+            statusText.textContent = `Magnetometer API not available`;
+        }
     }
 }
 //////////////////////////////////////////////////////////////////////////
--- magnetometer/src/script.ts
+++ magnetometer/src/script.ts
@@ -1,16 +1,40 @@
-let textout : HTMLElement | null;
+let statusText : HTMLElement | null;
 let mag : Magnetometer;
 
+function magnetometerReading() : void {
+  if (statusText) {
+    statusText.textContent = `Magnetometer Reading Callback: X=${mag.x} Y=${mag.y} Z=${mag.z} @ time=${mag.timestamp}`;
+  }
+}
+
+function magnetometerError(e:SensorErrorEvent) : void {
+  if (statusText) {
+    statusText.textContent = `Magnetometer Error Callback: ${e.error.message}`;
+  }
+}
+
 function contentLoaded() : void {
-  console.log("Content loaded!");
+  statusText = document.getElementById('statusText');
 
-  textout = document.getElementById('textout');
-
-  if (null !== textout) {
-    textout.textContent = `typeof(Gyroscope) is ${typeof(Gyroscope)} and typeof(Magnetometer) is ${typeof(Magnetometer)}`;
-
-    mag = new Magnetometer();
-    textout.textContent = `Magnetometer activated=${mag.activated} and hasReading=${mag.hasReading}`;
+  if (statusText) {
+    if ('undefined' !== typeof(Magnetometer)) {
+      try {
+        mag = new Magnetometer({frequency:10});
+        statusText.textContent = `Magnetometer Created`;
+        mag.onerror = magnetometerError;
+        mag.onreading = magnetometerReading;
+        mag.start();
+        statusText.textContent = `Magnetometer Started`;
+      } catch (e) {
+        if (e && typeof(e) === 'object' && 'message' in e) {
+          statusText.textContent = `Magnetometer Exception: ${e.message}`;
+        } else {
+          statusText.textContent = 'Magnetometer Exception raised with no diagnostic message';
+        }
+      }
+    } else {
+      statusText.textContent = `Magnetometer API not available`;
+    }
   }
 }
 
