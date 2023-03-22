3f0a0a7feae57fc7b7e1c74f27ea62e13d6c7f81
Regorlas <roger.random@outlook.com>
2023-03-22 03:48:20 +0000

    Set up TypeScript

---
+++ magnetometer/src/script.js
@@ -0,0 +1,8 @@
+"use strict";
+function contentLoaded() {
+    console.log("Content loaded!");
+}
+//////////////////////////////////////////////////////////////////////////
+//
+//  Page load setup
+document.addEventListener('DOMContentLoaded', contentLoaded, false);
---
+++ magnetometer/src/script.ts
@@ -0,0 +1,9 @@
+function contentLoaded() : void {
+  console.log("Content loaded!");
+}
+
+//////////////////////////////////////////////////////////////////////////
+//
+//  Page load setup
+
+document.addEventListener('DOMContentLoaded', contentLoaded, false);
