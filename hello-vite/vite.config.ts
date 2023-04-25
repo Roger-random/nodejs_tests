839ab6fd57db034c7a4493d8a65b0ed421fb2af2
Regorlas <roger.random@outlook.com>
2023-04-25 20:27:17 +0000

    Vite legacy build for IE11

--- hello-vite/vite.config.ts
+++ hello-vite/vite.config.ts
@@ -1,4 +1,11 @@
 /** @type {import('vite').UserConfig} */
+import legacy from '@vitejs/plugin-legacy'
+
 export default {
   "base":"/nodejs_tests",
+  plugins: [
+    legacy({
+      targets: ['defaults', 'IE 11'],
+    }),
+  ],
 }
